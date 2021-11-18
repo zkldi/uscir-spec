3f46145635ce2e79b7d018369e90f2f3c8fb36b9
ereti <43418802+ereti@users.noreply.github.com>
2021-11-18 14:29:32 +0000

    Bump version to 0.3.3-a

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, winter, zkldi'
 author = 'winter, zkldi'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.2-a'
+release = '0.3.3-a'
 
 # -- General configuration ---------------------------------------------------
 
