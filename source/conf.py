bfa5d98412262c095d63033fa229dc1169bb06b1
ereti <43418802+ereti@users.noreply.github.com>
2021-12-19 00:08:54 +0000

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, winter, zkldi'
 author = 'winter, zkldi'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.3-a'
+release = '0.4.0-a'
 
 # -- General configuration ---------------------------------------------------
 
